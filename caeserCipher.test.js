5fd50e0b0d04ebc32bd02c6382d909a8730ed2ef
rgremaud <rgremau@gmail.com>
2026-02-03 21:00:55 -0500

    Add caeser cipher

--- caeserCipher.test.js
+++ caeserCipher.test.js
@@ -2,6 +2,10 @@ import { caesarCipher } from "./caesarCipher"
 
 test("String Test", () => {
   const string = caesarCipher('xyz', 3);
+  const stringTwo = caesarCipher('x y z', 3);
+  const stringThree = caesarCipher("xy z!", 3);
   
   expect(string).toBe("abc");
+  expect(stringTwo).toBe("a b c")
+  expect(stringThree).toBe("ab c!")
 });
